cc78799d711d502f29844c34111a337e8395259a
Denis Kleptsov <denis@kleptsov.com>
2022-09-18 20:33:26 +0300

    Initial commit

---
+++ 01_data_model/01_card_deck.py
@@ -0,0 +1,53 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank in self.ranks
+                                        for suit in self.suits]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+def main():
+    beer_card = Card('7', 'diamonds')
+    print(beer_card)
+
+    deck = FrenchDeck()
+    print(len(deck))
+    print(deck[0])
+    print(deck[-1])
+    # for card in deck:
+    #     print(card)
+
+    print(choice(deck))
+
+    # Slicing of deck
+    print(deck[:3])
+    print(deck[12::13])
+    print(list(range(100))[12::13])
+
+    for card in reversed(deck):
+        print(card)
+
+    print(Card('7', 'beasts') in deck)
+
+
+    # Sorting
+def spades_high(card):
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+if __name__ == "__main__":
+    main()
